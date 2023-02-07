9071ea60319f05f23846b54cd0e3aee9bef8e916
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2023-02-07 14:00:33 -0300

    feat: initial structure.
    Signed-off-by: Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>

---
+++ README.md
@@ -0,0 +1,41 @@
+# action-compare-development-time
+
+## Description
+
+Action that returns the interval (in days) between the start of development and the opening of the Pull Request.
+
+## Requiremets
+
+- Personal Access Token
+
+## Usage for metrics through Git Flow
+
+1. Create directory `.github/worfklows` in the root of your project;
+
+2. Create file `compare.yaml` with content similar to the following:
+
+```yaml
+name: Compare
+on:
+  pull_request:
+    types: [opened, reopened, synchronize]
+    branches:
+      - main
+    
+jobs:
+  compare-time-development:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Compare Development Time
+        uses: padupe/action-compare-development-time@1.0.0
+        with:
+          githubToken: ${{ secrets.PAT_TOKEN }}
+```
+
+## General use
+
+```yaml
+        uses: padupe/action-compare-development-time@1.0.0
+        with:
+          githubToken: ${{ secrets.PAT_TOKEN }}
+```
---
+++ index.js
@@ -0,0 +1,80 @@
+const { Octokit } = require('@octokit/core')
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const githubToken = core.getInput('githubToken')
+
+const ocktokit = new Octokit({
+    auth: githubToken
+})
+
+async function run() {
+    try {
+        if (githubToken) {
+            const href = github.context.payload.ref.split('/')
+
+            let branchEvent
+
+            if (href.length > 2) {
+                branchEvent = `${href[1]}/${href[2]}`
+            } else {
+                branchEvent = href[1]
+            }
+
+            const branchDefault = github.context.payload.default_branch
+            
+            const getDateDefaultBranch = await getLastCommitBranchDefault(github.context.payload.repository.owner.login, github.context.payload.repository.name)
+            const getDateBranchEvent = await getLastCommitBranchBase(github.context.payload.repository.owner.login, github.context.payload.repository.name, branchEvent)
+
+            const interval = compareDate(getDateDefaultBranch.date, getDateBranchEvent.date)
+
+            core.setOutput(`This implementation has an interval of ${interval} days compared to branch ${branchDefault}.`)
+        } else {
+            core.setFailed(`"githubToken" is required!`)
+        }
+
+    } catch (error) {
+        core.setFailed(`Error at Action: ${error}`)
+    }
+}
+
+async function getLastCommitBranchDefault(repoOwner, repoName) {
+    const commits = await ocktokit.request('GET /repos/{owner}/{repo}/commits', {
+        owner: repoOwner,
+        repo: repoName
+    })
+
+    const lastCommit = commits.data[0]
+
+    return {
+        sha: lastCommit.sha,
+        author: lastCommit.commit.author.name,
+        date: lastCommit.commit.author.date
+    }
+}
+
+async function getLastCommitBranchBase(repoOwner, repoName, branchRef) {  
+    const commits = await ocktokit.request('GET /repos/{owner}/{repo}/commits/{ref}', {
+        owner: repoOwner,
+        repo: repoName,
+        ref: branchRef
+    })
+
+    const lastCommit = commits.data
+
+    return {
+        sha: lastCommit.sha,
+        author: lastCommit.commit.author.name,
+        date: lastCommit.commit.author.date
+    }
+}
+
+function compareDate(baseDate, lastDate) {
+    const base = new Date(baseDate)
+    const last = new Date(lastDate)
+
+    const difference = last.getTime() - base.getTime()
+    return Math.ceil(difference / (1000 * 3600 * 24))
+}
+
+run()
